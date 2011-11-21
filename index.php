fe526cb1ed93f1ec042368babc56bd9371274954
Eric Kemp-Benedict <eric.kemp-benedict@sei.org>
2011-11-21 17:15:54 +0000

    Figured out why it was sluggish--using non-default pathway, so it was calculating each time. [s:pivotal, t:21292803]

--- index.php
+++ index.php
@@ -43,8 +43,8 @@ if ($_POST) {
                 <li id="ambition">
                     <a class="definition" href="#">Level of Global Ambition</a>
                     <ul class="radio">
-                        <li><label><input type="radio" name="ambition" value="<?php echo $pathway_id['low'] ?>"/> <?php echo $pathway_label['low'] ?></label></li>
-                        <li><label><input type="radio" name="ambition" value="<?php echo $pathway_id['med'] ?>" checked="checked" /> <?php echo $pathway_label['med'] ?></label></li>
+                        <li><label><input type="radio" name="ambition" value="<?php echo $pathway_id['low'] ?>" checked="checked" /> <?php echo $pathway_label['low'] ?></label></li>
+                        <li><label><input type="radio" name="ambition" value="<?php echo $pathway_id['med'] ?>"/> <?php echo $pathway_label['med'] ?></label></li>
                         <li><label><input type="radio" name="ambition" value="<?php echo $pathway_id['high'] ?>"/> <?php echo $pathway_label['high'] ?></label></li>
                     </ul>
                 </li>
