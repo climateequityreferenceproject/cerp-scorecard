d789b49bf07caeedbf64da9d0582e0ba8e94c3f2
Eric Kemp-Benedict <eric.kemp-benedict@sei.org>
2012-01-02 12:36:34 +0000

    Added the @package tag to each file to avoid having "Welcome to default" pop up.

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@
  * 
  * PHP Version 5
  *
+ * @package GDRsScorecard
  * @copyright 2011-2012 EcoEquity and Stockholm Environment Institute
  * @license All rights reserved
  * @link http://www.gdrights.org/
