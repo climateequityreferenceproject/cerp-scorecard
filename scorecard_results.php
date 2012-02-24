70ba70523d49b865363e907a70dcfab6f2f0e65f
tkb <214300+tylerkb@users.noreply.github.com>
2012-02-24 14:55:13 +0000

    Added separate glossary link for 'more results for this country,' as the default hijacked the link to the calculator. Having trouble getting the new 'what is this?' link text close to the big link it refers to - will have to come back to that.
    Also tweaked the widths and padding of form and results slightly, in unsuccessful try to keep longest form label from wrapping. -tkb

--- scorecard_results.php
+++ scorecard_results.php
@@ -104,7 +104,11 @@ function getResults()
     }
     $calc_url = '"' . getCalcUrl($iso3) . '"';
     $retval .= '<h2 id="more_options"><a href=' . $calc_url . ' target="_blank">';
-    $retval .= $glossary->getLink('gloss_more_results', true) . ' &#187;</a></h2>';
+    $retval .= 'more results for this country';
+    $retval .= ' &#187;</a>';
+    $retval .= '<br /><span class="what"><a class="def_link" href="glossary.php#gloss_more_results" target="_blank">what is this?</a></span>';
+    $retval .= '</h2>';
+    // $retval .= $glossary->getLink('gloss_more_results', true);
     $retval .= '<div id="details">';
     $retval .= '<h2>Details about this pledge</h2>';
     $retval .= '<p class="first">' . $effort_array['pledge_description'];
