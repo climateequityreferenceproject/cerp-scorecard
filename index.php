c153d363b659d2391c34473a238f3a4bb639afd4
tomasvn <43656619+toma-ecoequity@users.noreply.github.com>
2013-12-06 00:37:41 +0000

    Updated the Acks

--- index.php
+++ index.php
@@ -205,8 +205,8 @@ if (isset($_POST['country']) && ($_POST['country']!=='none')) {
 
         <footer>
             <p class="first">Thanks to the <a href="http://www.minor-foundation.no/" target="_blank">Minor Foundation for Major Challenges</a>, and to the <a href="http://www.rbf.org/">Rockefeller Brothers Fund</a>, <a href="http://www.christianaid.org.uk/">Christian Aid</a>, and the <a href="http://www.sei-international.org/">Stockholm Environment Institute</a> for supporting this effort.</p>
-            <p><?php echo $glossary->getLink('sc_credits') ?></p>
-            <p>Please send feedback on the Climate Equity Pledge Scorecard to <a href='m&#97;il&#116;o&#58;f%65&#37;65db%61ck&#64;gdri&#103;&#104;ts%2E%6F&#114;&#103;'>feed&#98;ack&#64;gdrig&#104;&#116;s&#46;org</a></p>
+            <p>This Scorecard system is based on the Greenhouse Development Rights Equity Reference Framework. <?php echo $glossary->getLink('sc_credits') ?></p>
+            <p>Feedback welcome.  Please email it to <a href='m&#97;il&#116;o&#58;f%65&#37;65db%61ck&#64;gdri&#103;&#104;ts%2E%6F&#114;&#103;'>feed&#98;ack&#64;gdrig&#104;&#116;s&#46;org</a></p>
         </footer>
     </div> <!--! end of #container -->
 
