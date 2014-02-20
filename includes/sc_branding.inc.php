5b2538d0fb0a73a659e115ebf46f71c0b118c8f7
tkb <214300+tylerkb@users.noreply.github.com>
2014-02-20 14:07:53 +0000

    Added a gazillion long text links in the main nav menu

---
+++ includes/sc_branding.inc.php
@@ -0,0 +1,16 @@
+<?php
+if (isDev()) {
+    $scorecard_home_url = "http://gdrights.org/scorecard_dev/";
+} else {
+    $scorecard_home_url = "http://gdrights.org/scorecard/";
+}
+?>
+            <div id="header" class="group">
+                <h1><a href="<?php echo $scorecard_home_url;?>">Climate Equity Pledge Scorecard</a> <span>beta</span></h1>
+                <div id="partners">
+                    <ul>
+                        <li><a id="ecoequity" href="http://www.ecoequity.org">EcoEquity</a></li>
+                        <li><a id="seius" href="http://www.sei.se">SEI-US</a></li>
+                    </ul>
+                </div><!-- end #partners -->
+            </div><!-- end #header -->
