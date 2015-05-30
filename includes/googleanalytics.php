a4bf79221c85cf1b33b9551c5dca78239d0411ad
Christian Holz <725021+krueschan@users.noreply.github.com>
2015-05-30 14:02:33 +0000

    - we now put a 3 month cookie on the computers of users that use the _dev version of the scorecard and do not ever send tracking pings to google analytics if such a cookie exists on a users computer (even when accessing the public version of the scorecard)

--- includes/googleanalytics.php
+++ includes/googleanalytics.php
@@ -1,13 +1,23 @@
 <?php
-$ga_script = "\n";
-$ga_script .= "<script>";
-$ga_script .= "\n";
-$ga_script .= "  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');";
-$ga_script .= "\n";
-$ga_script .= "ga('create', 'UA-62836318-3', 'auto');";
-$ga_script .= "\n";
-$ga_script .= "  ga('send', 'pageview');";
-$ga_script .= "\n";
-$ga_script .= "</script>";
-$ga_script .= "\n";
-echo ($ga_script);
+
+// if a user uses the _dev version of the scorecard, we drop a cookie on her 
+// computer to identify them as developer, in which case the google analytics
+// tracking code gets omitted (even during his use of the public scorecard)
+if (strpos($_SERVER['PHP_SELF'], '_dev')) {
+    setcookie("this_user_is_developer","true",time()+60*60*24*365.25/4, "/", $host_name); // cookie life time = 1/4 of a year    
+}
+
+if (!($_COOKIE['this_user_is_developer']=='true')) {
+    $ga_script = "\n";
+    $ga_script .= "<script>";
+    $ga_script .= "\n";
+    $ga_script .= "  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');";
+    $ga_script .= "\n";
+    $ga_script .= "ga('create', 'UA-62836318-3', 'auto');";
+    $ga_script .= "\n";
+    $ga_script .= "  ga('send', 'pageview');";
+    $ga_script .= "\n";
+    $ga_script .= "</script>";
+    $ga_script .= "\n";
+    echo ($ga_script);
+}
